b2ceeab342d0d76e661157366a148bcfe48ae642
chickenbane <thechickenbane@gmail.com>
2017-03-05 20:44:24 -0800

    24

---
+++ app/src/test/kotlin/advent/Day24Test.kt
@@ -0,0 +1,35 @@
+package advent
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Created by joey on 3/5/17.
+ */
+class Day24Test {
+
+    @Test
+    fun example() {
+        val exampleNums = listOf(11, 9, 10, 8, 2, 7, 5, 4, 3, 1)
+        val third = exampleNums.sum() / 3
+        val g = Day24.split(exampleNums, 0, third, 1, 0)
+        if (g == null) {
+            Assert.fail("returned null")
+        } else {
+            Assert.assertEquals("two items", 2, g.size)
+            Assert.assertEquals("qe=99", 99, g.qe)
+        }
+    }
+
+    @Test
+    fun answer() {
+        val qe = Day24.answer()
+        Assert.assertEquals("my answer", 11846773891, qe)
+    }
+
+    @Test
+    fun answer2() {
+        val qe = Day24.answer2()
+        Assert.assertEquals("my answer", 80393059, qe)
+    }
+}
